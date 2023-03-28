05f6d92a0e05616487ddfd5e3a057ec77733583d
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 18:02:48 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
 resource "aws_elasticache_cluster" "elasticache" {
-  cluster_id           = "${var.env}-elasticache"
-  engine               = var.engine
-  engine_version       = var.engine_version
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  port                 = 6379
+  cluster_id        = "${var.env}-elasticache"
+  engine            = var.engine
+  node_type         = var.node_type
+  num_cache_nodes   = var.num_cache_nodes
+  engine_version    = var.engine_version
+  port              = 6379
   subnet_group_name = aws_elasticache_subnet_group.main.name
 
   tags = merge(
     var.tags,
-    { Name = "${var.env}-elsticache" }
+    { Name = "${var.env}-elasticache" }
   )
 }
 
-
-
 resource "aws_elasticache_subnet_group" "main" {
-  name = "${var.env}-elasticache"
+  name       = "${var.env}-rds"
   subnet_ids = var.subnet_ids
 
   tags = merge(
--- vars.tf
+++ vars.tf
@@ -5,4 +5,3 @@ variable "engine_version" {}
 variable "num_cache_nodes" {}
 variable "node_type" {}
 variable "subnet_ids" {}
-variable "elasticache" {}
